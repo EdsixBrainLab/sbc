bdf569002f689d51e7a55d25e2c6af1de57f0a92
EdsixBrainLab <51076723+EdsixBrainLab@users.noreply.github.com>
2025-12-08 11:20:49 +0530

    Add files via upload

---
+++ language/english/footer_lang.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['footeraddress'] = "Edsix Brain Lab Private LTD.<br/># 1 H, Module no. 8,<br/>IIT Madras Research Park First Floor,<br/>Kanagam Road ,Taramani Chennai - 113";
+
+$lang['ftjoin'] = "Join Us";
+$lang['ftphonenumber'] = "044-66469877";
+$lang['ftemail'] = "angel@skillangels.com";
+$lang['fthome'] = "Home";
+$lang['ftterms'] = "Terms Of Service";
+$lang['ftprivacy'] = "Privacy Policy";
+$lang['ftfaq'] = "FAQ";
+$lang['fttitle'] = "EdSix Brain Lab Pvt Ltd";
+$lang['allrights'] = "2017 Skillangels. All rights reserved";
+
+ 
+
+?>
---
+++ language/kannada/puzzle_lang.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$lang['msgdashboard'] = 'ಡ್ಯಾಶ್ಬೋರ್ಡ್ ಸ್ವಾಗತ';
+
+
+
+?>
