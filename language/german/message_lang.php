90d4ed313c695567c84442e428c681264289d83d
EdsixBrainLab <51076723+EdsixBrainLab@users.noreply.github.com>
2025-12-08 11:24:50 +0530

    Delete language directory

--- language/german/message_lang.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$lang['welcome_message'] = 'Willkommen in CodexWorld';
-?>
--- language/kannada/puzzle_lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-
-$lang['msgdashboard'] = 'ಡ್ಯಾಶ್ಬೋರ್ಡ್ ಸ್ವಾಗತ';
-
-
-
-?>
